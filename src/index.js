d02f5e0673a844a5cf821f12abac52756ed7c01a
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-06-13 12:41:07 +0530

    (1306) commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "tachyons";
-import CardList from "./CardList.js";
-import { robots } from "./robots.js";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>
 );
